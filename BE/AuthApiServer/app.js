59d996cc707a2215fc96e0af8f202bf941504ec7
leeshinyook <tlsdbr97@gmail.com>
2020-04-08 17:05:12 +0900

    Scaffold catch 404 error handler

--- BE/AuthApiServer/app.js
+++ BE/AuthApiServer/app.js
@@ -5,4 +5,10 @@ const app = express();
 require('./auth/passport').setup(app);
 // routing
 app.use('/', require('./routes/index'));
-app.use('/', require('./routes/index'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
