2f22522601c9ae14a1b2e0bc4d863ffcbc8bcc33
leeshinyook <tlsdbr97@gmail.com>
2020-04-09 00:44:01 +0900

    Set bootstrap

--- FE/presentation-web/src/components/Main.vue
+++ FE/presentation-web/src/components/Main.vue
@@ -1,5 +1,13 @@
 <template>
+<div id="app">
+  <div class="title">
 
+  </div>
+  <div>
+    <!-- Standard button -->
+<button type="button" class="btn btn-primary">Google 로그인</button>
+  </div>
+</div>
 </template>
 
 <script>
