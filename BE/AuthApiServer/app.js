6d9afd1784ac0ed0473cc54a91a4c5666a7c4d4e
leeshinyook <tlsdbr97@gmail.com>
2020-04-08 18:13:10 +0900

    Set express AuthApiserver

--- BE/AuthApiServer/app.js
+++ BE/AuthApiServer/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-// passport settings
-require('./auth/passport').setup(app);
 // routing
 app.use('/', require('./routes/index'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// passport settings
+require('./auth/passport.js').setup(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -12,3 +22,24 @@ app.use((req, res, next) => {
 	err.status = 404;
 	next(err);
 });
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+// require('./routes')(app);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`[AuthApiServer] Listening on Port ${PORT}`);
+});
+
+module.exports = app;
