d44dc29d31bf89ace175c4b497a467fa9270054c
leeshinyook <tlsdbr97@gmail.com>
2020-04-08 18:13:41 +0900

    Add feature a googleOAuth2

--- BE/AuthApiServer/auth/passport.js
+++ BE/AuthApiServer/auth/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const session = require('express-session');
-const config = require('./config.json');
+const config = require('../../config.json');
 
 const GOOGLE_CLIENT_ID = config.google.clientId;
 const GOOGLE_CLIENT_SECRET = config.google.clientSecret;
@@ -24,32 +24,43 @@ passport.use(
 			callbackURL: 'http://localhost:3000/auth/google/callback'
 		},
 		function(accessToken, refreshToken, profile, done) {
-			// async verification
+			/* 
+				req.session.passport 정보 저장.
+				done메소드에 전달된 정보가 세션에 저장된다.
+				profile을 이요해서 사용자 정보를 DB.
+			*/
 			process.nextTick(function() {
-				return done(null, profile);
+				user = profile;
+				return done(null, user);
 			});
 		}
 	)
 );
 
 const setup = function(app) {
-	app.use(session({ secret: 'keyboard cat' }));
-	app.use(passport.initialize());
-	app.use(passport.session());
-	app.get(
-		'/auth/google',
-		passport.authenticate('google', { scope: [ 'openid', 'email' ] }, function(req, res) {
-			/*
-        The request will be redirected to Google for authentication
-        function will not be called.
-     */
+	app.use(
+		session({
+			secret: 'keyboard cat',
+			resave: true,
+			saveUninitialized: true
 		})
 	);
-	app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+	app.use(passport.initialize());
+	app.use(passport.session());
+	app.get('/auth/google', passport.authenticate('google', { scope: [ 'openid', 'email' ] }), function(req, res) {
+		// The request will be redirected to Google for authentication, so this
+		// function will not be called.
+	});
+
+	app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(
+		req,
+		res
+	) {
 		console.log(req.query);
 		res.redirect('/');
 	});
-	app.get('/logout', (req, res) => {
+
+	app.get('/logout', function(req, res) {
 		req.logout();
 		res.redirect('/login');
 	});
