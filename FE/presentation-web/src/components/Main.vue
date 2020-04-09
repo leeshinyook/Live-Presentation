bf3d412361d1826ba28daa64b7ce0c84a8204bdc
leeshinyook <tlsdbr97@gmail.com>
2020-04-09 21:33:22 +0900

    Set connection BE using axios

--- FE/presentation-web/src/components/Main.vue
+++ FE/presentation-web/src/components/Main.vue
@@ -5,14 +5,23 @@
   </div>
   <div>
     <!-- Standard button -->
-<button type="button" class="btn btn-primary"><i class="fa fa-google" aria-hidden="true"></i>oogle Login</button>
+<a href="http://localhost:3000/auth/google"><button type="button" class="btn btn-primary" @click="LoginGoogle"><i class="fa fa-google" aria-hidden="true"></i>oogle Login</button></a>
   </div>
 </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
 
+    }
+  },
+  methods: {
+    LoginGoogle() {
+
+    }
+  }
 }
 </script>
 
