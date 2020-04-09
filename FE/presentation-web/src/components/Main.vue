213971e01194230bea0a1db8c2f989134172ba59
leeshinyook <tlsdbr97@gmail.com>
2020-04-10 01:10:35 +0900

    Implement simple markup a intropage

--- FE/presentation-web/src/components/Main.vue
+++ FE/presentation-web/src/components/Main.vue
@@ -5,7 +5,19 @@
   </div>
   <div>
     <!-- Standard button -->
-<a href="http://localhost:3000/auth/google"><button type="button" class="btn btn-primary" @click="LoginGoogle"><i class="fa fa-google" aria-hidden="true"></i>oogle Login</button></a>
+    <div>
+      <div>익명으로 질문해주세요!</div>
+        <input type="text" class="form-control" placeholder="이벤트 코드입력하기">
+        <button type="button" class="btn btn-primary btn-lg btn-block">입장하기</button>
+
+
+    </div>
+    <div>이벤트를 만들고 싶다면, 로그인을 해주세요!</div>
+    <div>
+<a href="/api/auth/google"><button type="button" class="btn btn-primary" @click="LoginGoogle"><i class="fa fa-google" aria-hidden="true"></i>oogle Login</button></a>
+    </div>
+
+
   </div>
 </div>
 </template>
@@ -19,7 +31,6 @@ export default {
   },
   methods: {
     LoginGoogle() {
-
     }
   }
 }
