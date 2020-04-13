4daa49baa28c1325d2bb4dc87047c8e6cfd04e18
leeshinyook <tlsdbr97@gmail.com>
2020-04-13 23:53:59 +0900

    Add dependency socket.io

--- BE/ChatApiServer/app.js
+++ BE/ChatApiServer/app.js
@@ -7,6 +7,8 @@ const path = require('path');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 app.use(cors());
 app.use('/', indexRouter);
--- BE/ChatApiServer/routes/index.js
+++ BE/ChatApiServer/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../auth');
 
 router.get('/', (req, res, next) => {
-	console.log('CHATindex');
+	console.log('Test');
 });
 
 module.exports = router;
