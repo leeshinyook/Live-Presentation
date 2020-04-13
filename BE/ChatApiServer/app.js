24a79b6393be618f2edef9d5316f8c2b9e8a80e9
leeshinyook <tlsdbr97@gmail.com>
2020-04-13 23:45:15 +0900

    Set ChatAPI server port on 3001

---
+++ BE/ChatApiServer/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const app = express();
+
+app.use(cors());
+app.use('/', indexRouter);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+const PORT = 3001;
+app.listen(PORT, () => {
+	console.log(`[ChatApiServer] Listening on Port ${PORT}`);
+});
+
+module.exports = app;
