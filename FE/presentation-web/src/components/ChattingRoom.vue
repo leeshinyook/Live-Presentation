b4f565cd65825e36a6ed41400a4988a7d56ed950
leeshinyook <tlsdbr97@gmail.com>
2020-04-17 02:10:37 +0900

    Add cryptoFlag to not generate roomNumber twice

--- FE/presentation-web/src/components/ChattingRoom.vue
+++ FE/presentation-web/src/components/ChattingRoom.vue
@@ -6,6 +6,7 @@
   </header>
   <h1>호스트</h1>
   <button v-on:click="CreateRoom()">방만들기</button>
+  <div v-if="this.user.uniqueNumber">고유번호 : {{user.uniqueNumber}}</div>
 </div>
 </template>
 
@@ -14,7 +15,6 @@ const CryptoJS = require('crypto-js');
 export default {
   created() {
     this.$axios.get("/api/account").then(res => {
-      console.log(res.data);
       this.user.name = res.data.name;
       this.user.picture = res.data.picture;
       this.user.email = res.data.email;
@@ -25,15 +25,18 @@ export default {
       user: {
         name: '',
         picture: '',
-        email: ''
-      }
+        email: '',
+        uniqueNumber: ''
+      },
+      cryptoFlag: false
     }
   },
   methods: {
     CreateRoom() {
-      console.log("makeRoom")
-      var userUniqueNumber = CryptoJS.AES.encrypt(JSON.stringify(this.user.email), 'keyboardcat').toString().substr(0, 15);
-      console.log(userUniqueNumber);
+      if(this.cryptoFlag) return;
+      var userUniqueNumber = CryptoJS.AES.encrypt(JSON.stringify(this.user.email), 'keyboardcat').toString().substr(0, 12);
+      this.user.uniqueNumber = userUniqueNumber;
+      this.cryptoFlag = true;
     }
   }
 }
