0232ba246c3d3264551d46d075fa16878901d84a
leeshinyook <tlsdbr97@gmail.com>
2020-04-17 02:01:14 +0900

    Add CryptoModule for setting userNumber

--- FE/presentation-web/src/components/ChattingRoom.vue
+++ FE/presentation-web/src/components/ChattingRoom.vue
@@ -5,26 +5,35 @@
   <strong>{{user.name}}</strong>님 안녕하세요!
   </header>
   <h1>호스트</h1>
-
+  <button v-on:click="CreateRoom()">방만들기</button>
 </div>
 </template>
 
 <script>
+const CryptoJS = require('crypto-js');
 export default {
   created() {
     this.$axios.get("/api/account").then(res => {
       console.log(res.data);
       this.user.name = res.data.name;
       this.user.picture = res.data.picture;
-
+      this.user.email = res.data.email;
     })
   },
   data() {
     return {
       user: {
         name: '',
-        picture: ''
+        picture: '',
+        email: ''
       }
+    }
+  },
+  methods: {
+    CreateRoom() {
+      console.log("makeRoom")
+      var userUniqueNumber = CryptoJS.AES.encrypt(JSON.stringify(this.user.email), 'keyboardcat').toString().substr(0, 15);
+      console.log(userUniqueNumber);
     }
   }
 }
