e5441798811fbaa2b7193fed817028936f527539
leeshinyook <tlsdbr97@gmail.com>
2020-04-26 00:04:03 +0900

    Set Redis pub, sub model

--- BE/ChatApiServer/app.js
+++ BE/ChatApiServer/app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const indexRouter = require('./routes/index');
 const app = require('express')();
 const server = require('http').createServer(app);
+const redis = require('redis');
 const io = require('socket.io')(server);
 
 app.use(cors());
@@ -35,11 +36,32 @@ if (app.get('env') === 'development') {
 	});
 }
 
+// 컨텍스트를 공유하기위해, redis store를 바인딩.
+// io.adapter(redis({ host: 'localhost', port: 6379 }));
+let store = redis.createClient(6379, 'localhost');
+let pub = redis.createClient(6379, 'localhost');
+let sub = redis.createClient(6379, 'localhost');
+sub.subscribe('sub');
+sub.on('subscribe', function(channel, count) {
+	console.log('Subscribed to ' + channel + '. Now subsribed to ' + count + 'channel(s).');
+});
+sub.on('message', function(channel, data) {
+	data = JSON.parse(data);
+	console.log(data);
+	if (data.sendType === 'sendToAllClientsInRoom') {
+		io.sockets.in(data.roomId).emit('message', { message: 'JOIN IN ROOM!', roomId: data.roomId });
+	}
+});
 io.on('connection', (socket) => {
 	console.log('a user connected' + socket);
 	socket.on('join', (data) => {
+		let reply = JSON.stringify({
+			roomId: data,
+			sendType: 'sendToAllClientsInRoom'
+		});
 		console.log('join room number : ' + data);
 		socket.join(data);
+		pub.publish('sub', reply);
 	});
 	socket.on('message', (data) => {
 		let reply = {
