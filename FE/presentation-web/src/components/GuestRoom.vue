c93961fd991b34decd01d990e4547622cc54cd9c
leeshinyook <tlsdbr97@gmail.com>
2020-05-16 00:28:33 +0900

    Implement GuestView publishing

--- FE/presentation-web/src/components/GuestRoom.vue
+++ FE/presentation-web/src/components/GuestRoom.vue
@@ -1,13 +1,41 @@
 <template>
 <div>
-  <div>
+  <div v-if="!registerFlag">
+    <div>
     <h1>게스트 모드</h1>
-    <h1>{{hostName}}님의 이벤트!</h1>
-    <input type="text" v-model="message">
-    <button @click="sendMessage()">Send</button>
+    <h3>{{hostName}}님의 이벤트</h3>
   </div>
-  <div>
-    <textarea v-model="textarea" disabled ></textarea>
+  <ul class="board">
+    <li v-for="(log, idx) in logs" :key="idx">
+      <div class="board_table">
+        <div class="author">
+          <i class="fa fa-user-circle-o" aria-hidden="true"></i> {{log.nickName}}
+        </div>
+        <div class="message">
+          {{log.message}}
+        </div>
+      </div>
+
+    </li>
+  </ul>
+
+    <div class="position-sticky">
+    <button class="btn btn-primary" @click="Regist()">글등록하기</button>
+    </div>
+  </div>
+  <div v-else id="register_view">
+    <div class="header">
+      <button @click="Regist()"><i class="fa fa-arrow-left" aria-hidden="true"></i></button>
+    </div>
+     <div class="form-group">
+      <label for="nickName">닉네임</label>
+      <input type="text" class="form-control" v-model="register.nickname" id="nickName" placeholder="Your Nickname">
+    </div>
+    <div class="form-group">
+      <label for="content">질문내용</label>
+      <textarea type="text" class="form-control" id="content" placeholder="질문내용을 입력해주세요" rows="2" v-model="register.message"></textarea>
+    </div>
+    <button class="btn btn-primary" @click="sendMessage()">질문하기</button>
   </div>
 </div>
 
@@ -22,26 +50,74 @@ export default {
       this.hostName = data;
     })
     this.$socket.on('chat', (data) => {
+      this.logs.push(data);
       this.textarea += data.message + '\n';
     })
   },
   data() {
     return {
-      message: '',
       textarea: '',
       roomNumber: '',
-      hostName: ''
+      hostName: '',
+      registerFlag : false,
+      register : {
+        nickname: '',
+        message: '',
+      },
+      logs: []
     }
   },
   methods: {
+    reset() {
+      console.log("asdf");
+      this.register.message = '';
+      this.register.nickname = '';
+    },
     sendMessage() {
-      this.$socket.emit('chat', {message : this.message, roomId: this.roomNumber});
+      let msg = {
+        message: this.register.message,
+        roomId: this.roomNumber,
+        nickName: this.register.nickname
+      }
+      this.$socket.emit('chat', msg);
       this.message = '';
+      this.Regist();
+      this.reset();
+    },
+    Regist() {
+      if(this.registerFlag) this.registerFlag = false;
+      else this.registerFlag = true;
     }
+
   }
 }
 </script>
 
 <style>
-
+ul {
+  list-style: none;
+}
+.board {
+  padding: 0%
+}
+.board_table {
+  border: 1px solid blue;
+  border-radius: 5px;
+  margin: 5px;
+  padding: 5px;
+  font-size: 16px;
+  text-align: left;
+}
+.board_table .author {
+  font-size: 20px;
+}
+.board_table .message {
+  padding: 5px;
+}
+#register_view .header {
+  margin: 0;
+  padding-left: 20px;
+  text-align: left;
+  font-size: 25px;
+}
 </style>
