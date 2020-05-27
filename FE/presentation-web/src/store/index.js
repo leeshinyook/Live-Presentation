6bb487535130daaf15fa888018730e39e170c080
leeshinyook <tlsdbr97@gmail.com>
2020-05-27 23:27:59 +0900

    Fix: vuex mutation bug

--- FE/presentation-web/src/store/index.js
+++ FE/presentation-web/src/store/index.js
@@ -1,31 +1,29 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    roomNum: "",
-    resultPollFlag: false
-  },
-  mutations: {
-    setRoomNumber: function(state, payload) {
-      state.roomNum = payload;
-    }
-  },
-  getters: {
-    getRoomNumber: function(state) {
-      return state.roomNum;
-    },
-    getPollFlag: function(state) {
-      return state.resultPollFlag;
-    }
-  },
-  mutations: {
-    changePollFlag: function() {
-      state.resultPollFlag = !state.resultPollFlag;
-    }
-  }
+	state: {
+		roomNum: '',
+		resultPollFlag: false
+	},
+	mutations: {
+		setRoomNumber: function(state, payload) {
+			state.roomNum = payload;
+		},
+		changePollFlag: function() {
+			state.resultPollFlag = !state.resultPollFlag;
+		}
+	},
+	getters: {
+		getRoomNumber: function(state) {
+			return state.roomNum;
+		},
+		getPollFlag: function(state) {
+			return state.resultPollFlag;
+		}
+	}
 });
 
 export default store;
