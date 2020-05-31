6774c8a68c8cdd9b2e73d841e82d481639389795
leeshinyook <tlsdbr97@gmail.com>
2020-06-01 01:00:59 +0900

    Add: GuestRoom UI 퍼블리싱

--- FE/presentation-web/src/components/GuestRoom.vue
+++ FE/presentation-web/src/components/GuestRoom.vue
@@ -2,10 +2,9 @@
   <div>
     <div v-if="!registerFlag">
       <div>
-        <h1>게스트 모드</h1>
         <h3>{{hostName}}님의 이벤트</h3>
         <span v-if="pollFlag">
-          <i class="fa fa-bell" aria-hidden="true"></i> 실시간 투표가 등록되었습니다.
+          <div class="poll_alarm"><i class="fa fa-bell" aria-hidden="true"></i> 실시간 투표가 등록되었습니다.</div>
           <div class="poll_table">
             <div class="poll_title">{{polls[0].pollTitle}}</div>
             <ul>
@@ -19,7 +18,7 @@
                 </div>
               </li>
             </ul>
-            <button class="btn btn-primary" @click="submitPoll()">제출하기</button>
+            <button class="btn btn-primary" @click="submitPoll()" id="submit_button">제출하기</button>
           </div>
         </span>
       </div>
@@ -31,7 +30,7 @@
               {{log.nickName}}
             </div>
             <div class="message">{{log.message}}</div>
-            <div><button @click="Like(idx)"><i class="fa fa-heart" aria-hidden="true"></i></button>
+            <div><i class="fa fa-heart" aria-hidden="true" @click="Like(idx)"></i>
               {{log.likeCnt}}
             </div>
           </div>
@@ -39,7 +38,7 @@
       </ul>
 
       <div class="position-sticky">
-        <button class="btn btn-primary" @click="Regist()">글등록하기</button>
+        <button class="btn btn-primary" id="register_button" @click="Regist()">질문등록하기</button>
       </div>
     </div>
     <div v-else id="register_view">
@@ -49,7 +48,7 @@
         </button>
       </div>
       <div class="form-group">
-        <label for="nickName">닉네임</label>
+        <label for="nickName" class="labeling">닉네임</label>
         <input
           type="text"
           class="form-control"
@@ -59,17 +58,17 @@
         />
       </div>
       <div class="form-group">
-        <label for="content">질문내용</label>
+        <label for="content" class="labeling">질문내용</label>
         <textarea
           type="text"
           class="form-control"
           id="content"
           placeholder="질문내용을 입력해주세요"
-          rows="2"
+          rows="5"
           v-model="register.message"
         ></textarea>
       </div>
-      <button class="btn btn-primary" @click="sendMessage()">질문하기</button>
+      <button class="btn btn-primary" @click="sendMessage()" id="submit_button">질문하기</button>
     </div>
   </div>
 </template>
@@ -174,14 +173,50 @@ export default {
 </script>
 
 <style>
+#register_button {
+  font-size: 20px;
+  letter-spacing: 2px;
+}
+#submit_button {
+  font-size: 17px;
+  letter-spacing: 2px;
+
+}
 ul {
   list-style: none;
   padding: 0;
 }
+h3 {
+  margin-top: 0;
+  padding: 20px;
+  background-color: rgb(55, 123, 181);
+  letter-spacing: 4px;
+  box-shadow: 4px 4px 4px 4px rgb(230, 230, 230);
+  color: rgb(240, 240, 240);
+}
+textarea {
+  padding: 0 2px 0 2px;
+}
+.labeling {
+  font-size: 17px;
+  letter-spacing: 3px;
+}
+.poll_alarm {
+  font-size: 17px;
+}
+.like_button {
+  background: rgb(255, 255, 255);
+  border: 0;
+}
+.header button {
+  margin-top: 20px;
+}
 .poll_table {
-  border: 1px solid blue;
+  background-color: rgb(255, 255, 255);
+  box-shadow: 4px 4px 4px 4px rgb(150, 150, 150);
   border-radius: 5px;
-  margin: 7px;
+  margin: 20px;
+  padding: 5px;
 }
 .poll_list {
   margin-left: 0;
@@ -191,8 +226,11 @@ ul {
   font-size: 25px;
 }
 .poll_content {
+  background-color: rgb(230, 230, 230);
+  font-weight: bolder;
+  box-shadow: 1px 1px 1px 1px rgb(150, 150, 150);
   font-size: 17px;
-  border: 1px solid blue;
+  padding: 4px;
   border-radius: 5px;
   margin: 5px;
 }
@@ -200,10 +238,12 @@ ul {
   padding: 0%;
 }
 .board_table {
-  border: 1px solid blue;
+  border: 1px solid rgb(240, 240, 240);
+  box-shadow: 3px 3px 3px 3px rgb(150, 150, 150);
+  background: rgb(255, 255, 255);
   border-radius: 5px;
-  margin: 5px;
-  padding: 5px;
+  margin: 10px;
+  padding: 10px;
   font-size: 16px;
   text-align: left;
 }
